295fda974c2660023d5fb7ee6b98dafde68bc8c6
Ryan Upham <rlupham@ncsu.edu>
2016-09-23 22:02:32 -0400

    Thinning and Hough transform are much faster. Changed some small things to make the code more pretty.

--- points.py
+++ points.py
@@ -1,3 +1,4 @@
+from itertools import product
 from math import ceil, hypot, cos, radians, sin
 
 
@@ -7,7 +8,6 @@ def distance(p1, p2):
 
 def thin(points):
     points = points[:]
-    pl = len(points)
     width, height = 0, 0
 
     for point in points:
@@ -19,19 +19,19 @@ def thin(points):
     width += 2
     height += 2
 
+    check = [(0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1)]
+
     pixels = [[False for x in range(0, width + 1)] for y in range(0, height + 1)]
-    marked = [[False for x in range(0, width + 1)] for y in range(0, height + 1)]
+    marked = set()
 
     for x, y in points:
         pixels[y][x] = True
 
     for x, y in points:
-        for tx in range(-1, 2):
-            for ty in range(-1, 2):
-                if not pixels[y + ty][x + tx]:
-                    marked[y][x] = True
-
-    check = [(0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1)]
+        for tx, ty in check:
+            if not pixels[y + ty][x + tx]:
+                marked.add((x, y))
+                break
 
     trimmed = True
     flags = []
@@ -39,84 +39,72 @@ def thin(points):
     while trimmed:
         flags.clear()
 
-        ind = 0
-
-        while ind < pl:
+        for x, y in marked:
             count = 0
             trans = 0
-            x, y = points[ind]
 
-            if not marked[y][x]:
-                ind += 1
-                continue
+            if pixels[y - 1][x]:
+                count += 1
 
-            for i in range(len(check)):
+            for i in range(1, 8):
                 if pixels[y + check[i][1]][x + check[i][0]]:
                     count += 1
-                if i > 0:
-                    if pixels[y + check[i][1]][x + check[i][0]] != pixels[y + check[i - 1][1]][x + check[i - 1][0]]:
+                    if not pixels[y + check[i - 1][1]][x + check[i - 1][0]]:
                         trans += 1
+                elif pixels[y + check[i - 1][1]][x + check[i - 1][0]]:
+                    trans += 1
 
-            if (2 <= count <= 6) and ceil(trans / 2) == 1:
+            if (2 <= count <= 6) and (trans == 1 or trans == 2):
                 if pixels[y + check[4][1]][x + check[4][0]] == False or \
                    pixels[y + check[2][1]][x + check[2][0]] == False or \
                    (pixels[y + check[0][1]][x + check[0][0]] == False and
                    (pixels[y + check[2][1]][x + check[2][0]] == False or
                    pixels[y + check[6][1]][x + check[6][0]] == False)):
                     flags.append((x, y))
-                    pl -= 1
-                    points[ind] = points[pl]
-                    ind -= 1
 
-                    for tx in range(-1, 2):
-                        for ty in range(-1, 2):
-                            marked[y + ty][x + tx] = True
+        for x, y in flags:
+            pixels[y][x] = False
 
-            ind += 1
-
-        for ind in flags:
-            pixels[ind[1]][ind[0]] = False
+        for x, y in flags:
+            for tx, ty in check:
+                if pixels[y + ty][x + tx]:
+                    marked.add((x + tx, y + ty))
 
         trimmed = len(flags) > 0
         flags.clear()
 
-        ind = 0
-
-        while ind < pl:
+        for x, y in marked:
             count = 0
             trans = 0
-            x, y = points[ind]
 
-            if not marked[y][x]:
-                ind += 1
-                continue
+            if pixels[y - 1][x]:
+                count += 1
 
-            for i in range(len(check)):
+            for i in range(1, 8):
                 if pixels[y + check[i][1]][x + check[i][0]]:
                     count += 1
-                if i > 0:
-                    if pixels[y + check[i][1]][x + check[i][0]] != pixels[y + check[i - 1][1]][x + check[i - 1][0]]:
+                    if not pixels[y + check[i - 1][1]][x + check[i - 1][0]]:
                         trans += 1
+                elif pixels[y + check[i - 1][1]][x + check[i - 1][0]]:
+                    trans += 1
 
-            if (2 <= count <= 6) and ceil(trans / 2) == 1:
+            if (2 <= count <= 6) and (trans == 1 or trans == 2):
                 if pixels[y + check[0][1]][x + check[0][0]] == False or \
                    pixels[y + check[6][1]][x + check[6][0]] == False or \
                    (pixels[y + check[4][1]][x + check[4][0]] == False and
                    (pixels[y + check[2][1]][x + check[2][0]] == False or
                    pixels[y + check[6][1]][x + check[6][0]] == False)):
                     flags.append((x, y))
-                    pl -= 1
-                    points[ind] = points[pl]
-                    ind -= 1
 
-                    for tx in range(-1, 2):
-                        for ty in range(-1, 2):
-                            marked[y + ty][x + tx] = True
+        for x, y in flags:
+            pixels[y][x] = False
 
-            ind += 1
+        marked.clear()
 
-        for ind in flags:
-            pixels[ind[1]][ind[0]] = False
+        for x, y in flags:
+            for tx, ty in check:
+                if pixels[y + ty][x + tx]:
+                    marked.add((x + tx, y + ty))
 
         trimmed = trimmed or len(flags) > 0
 
@@ -131,21 +119,24 @@ def thin(points):
 
 
 def hough(points):
+    sin_lookup = [sin(radians(theta)) for theta in range(-90, 90)]
+    cos_lookup = [cos(radians(theta)) for theta in range(-90, 90)]
+
     width, height = 0, 0
 
-    for point in points:
-        if point[0] > width:
-            width = point[0]
-        if point[1] > height:
-            height = point[1]
+    for x, y in points:
+        if x > width:
+            width = x
+        if y > height:
+            height = y
 
     max_dist = ceil(hypot(width, height))
     accum = [[0 for x in range(-90, 90)] for y in range(-max_dist, max_dist + 1)]
 
-    for point in points:
-        for theta in range(-90, 90):
-            r = round(point[0] * cos(radians(theta)) + point[1] * sin(radians(theta)))
-            accum[r + max_dist][theta + 90] += 1
+    for x, y in points:
+        for theta in range(180):
+            r = round(x * cos_lookup[theta] + y * sin_lookup[theta])
+            accum[r + max_dist][theta] += 1
 
     return accum
 
